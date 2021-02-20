55fbf89657ce0f7c6f15d78eee83a862e28bdad3
Amir Arif <xtremusphone@gmail.com>
2021-02-20 20:28:38 +0800

    Updates

---
+++ precache-manifest.971c1318802f1e229dab2eb3a9493962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2567c7cf9c7b413f514b",
+    "url": "/hutang/static/css/main.9849dd65.chunk.css"
+  },
+  {
+    "revision": "2567c7cf9c7b413f514b",
+    "url": "/hutang/static/js/main.2567c7cf.chunk.js"
+  },
+  {
+    "revision": "98596c9a1824c3586e59",
+    "url": "/hutang/static/js/1.98596c9a.chunk.js"
+  },
+  {
+    "revision": "da71e889cf873e33a03b",
+    "url": "/hutang/static/js/2.da71e889.chunk.js"
+  },
+  {
+    "revision": "77a25b0067d1566bc4ec",
+    "url": "/hutang/static/js/runtime~main.77a25b00.js"
+  },
+  {
+    "revision": "f6ff218af2956a4121a3f924181948e9",
+    "url": "/hutang/static/media/Blanchope.f6ff218a.ttf"
+  },
+  {
+    "revision": "521d0112cf328b97d47497298860f3c4",
+    "url": "/hutang/index.html"
+  }
+];
